e53e56a188bb41c5436cd4d415f3331b17877817
Bernard Dwyer -Beaudet <bernarddwyerb@gmail.com>
2021-03-12 15:14:17 -0500

    tit message

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,7 +4,13 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyCaPCa7QOYW0H1vOLA1Au-3qx6Fl7WalOU",
+  authDomain: "panier-achats-bdb.firebaseapp.com",
+  projectId: "panier-achats-bdb",
+  storageBucket: "panier-achats-bdb.appspot.com",
+  messagingSenderId: "670733747915",
+  appId: "1:670733747915:web:84b3004983ee4e13a2bd4e",
+  measurementId: "G-CX1RX1RHFC"
 };
 
 // Initialiser Firebase
@@ -16,3 +22,4 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
