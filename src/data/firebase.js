bd86b303355f501cba48de6021a8ab4c5381de15
Bernard Dwyer -Beaudet <bernarddwyerb@gmail.com>
2021-03-12 15:58:01 -0500

    delete config

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  // apiKey: "AIzaSyCaPCa7QOYW0H1vOLA1Au-3qx6Fl7WalOU",
-  // authDomain: "panier-achats-bdb.firebaseapp.com",
-  // projectId: "panier-achats-bdb",
-  // storageBucket: "panier-achats-bdb.appspot.com",
-  // messagingSenderId: "670733747915",
-  // appId: "1:670733747915:web:84b3004983ee4e13a2bd4e",
-  // measurementId: "G-CX1RX1RHFC"
+ 
 };
 
 // Initialiser Firebase
